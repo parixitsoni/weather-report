332723b9460b5add625e820e0c84f366c1c3aa6e
PARIXIT SONI <parikshitsoni85@gmail.com>
2020-09-16 11:16:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function init(resultFromServer){
     let cityHeader = document.getElementById('cityHeader');
     let weatherIcon = document.getElementById('documentIconImg');
 
-    weatherIcon.src = 'https://cors-anywhere.herokuapp.com/http://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + ".png";
+    weatherIcon.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + ".png";
     let resultDescription = resultFromServer.weather[0].description;
     weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
     temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176';
